7d83049d938302e9daac70a8c16d9713af5f289d
cernandes <caio-ernandes@hotmail.com>
2021-03-06 14:03:06 -0300

    refactoring de layout do grafico e organizacao de codigo

--- app/src/main/java/com/example/mpandroidchart/MainActivity.kt
+++ app/src/main/java/com/example/mpandroidchart/MainActivity.kt
@@ -3,64 +3,88 @@ package com.example.mpandroidchart
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.components.Legend
+import android.widget.Toast
+import com.example.mpandroidchart.Constants.FIXED_INCOME
+import com.example.mpandroidchart.Constants.FUNDS
+import com.example.mpandroidchart.Constants.PRIVATE_PENSION
+import com.example.mpandroidchart.Constants.VARIABLE_INCOME
+import com.example.mpandroidchart.databinding.ActivityMainBinding
+import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.github.mikephil.charting.utils.ColorTemplate
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setupClickListener()
         setupPieChart()
     }
 
-    private fun setupClickListener(){
-       btnContinue.setOnClickListener {
-           SecondScreen.newInstance(this, 4000f, 6000F)
-       }
+    private fun setupClickListener() = with(binding) {
+        mainPieChart.setOnChartValueSelectedListener(object : OnChartValueSelectedListener {
+            override fun onValueSelected(e: Entry?, h: Highlight?) {
+                showToast((e as PieEntry).label)
+            }
+
+            override fun onNothingSelected() {
+                showToast()
+            }
+        })
+        btnContinue.setOnClickListener {
+            SecondScreenActivity.newInstance(
+                context = this@MainActivity,
+                totalValue = 4000f,
+                investmentValue = 6000F
+            )
+        }
     }
 
     private fun setupPieChart() {
         setupPieChartConfigs()
-        mainPieChart.data = getPieData()
+        binding.mainPieChart.data = getPieData()
     }
 
     private fun setupPieChartConfigs() {
-        mainPieChart.isDrawHoleEnabled = true
-        mainPieChart.setHoleColor(Color.TRANSPARENT)
-        mainPieChart.setDrawEntryLabels(false)
-        mainPieChart.holeRadius = 85f
-        mainPieChart.description.isEnabled = false
-        mainPieChart.legend.isEnabled = false
-        mainPieChart.renderer.paintRender.setShadowLayer(10f, 0f, 24f, ContextCompat.getColor(applicationContext, android.R.color.darker_gray)
-        )
+        binding.mainPieChart.apply {
+            isDrawHoleEnabled = true
+            setHoleColor(Color.TRANSPARENT)
+            setDrawEntryLabels(false)
+            holeRadius = 85f
+            description.isEnabled = false
+            legend.isEnabled = false
+        }
     }
 
-    private fun getPieData(): PieData {
-        val pieDataSet = PieDataSet(getPieDataSet(), "")
-        pieDataSet.setDrawValues(false)
-        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS.toList())
-        pieDataSet.sliceSpace = 3f
-        return PieData(pieDataSet)
-    }
+    private fun getPieData(): PieData = PieData(PieDataSet(getPieDataSet(), "").apply {
+        setDrawValues(false)
+        colors = ColorTemplate.JOYFUL_COLORS.toList()
+        sliceSpace = 3f
+    })
 
     private fun getPieDataSet(): ArrayList<PieEntry> {
-        val pieChartEntries = arrayListOf<PieEntry>()
-        pieChartEntries.add(PieEntry(8000F, "Renda Fixa"))
-        pieChartEntries.add(PieEntry(8000F, "Renda Variável"))
-        pieChartEntries.add(PieEntry(8000F, "Fundos"))
-        pieChartEntries.add(PieEntry(8000F, "Previdência Privada"))
-        return pieChartEntries
+        return arrayListOf<PieEntry>().apply {
+            add(PieEntry(8000F, FIXED_INCOME))
+            add(PieEntry(8000F, VARIABLE_INCOME))
+            add(PieEntry(8000F, FUNDS))
+            add(PieEntry(8000F, PRIVATE_PENSION))
+        }
     }
 
-
+    private fun showToast(msg: String? = null) {
+        if (msg != null) {
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, R.string.nothing_selected, Toast.LENGTH_LONG).show()
+        }
+    }
 }
---
+++ app/src/main/java/com/example/mpandroidchart/SecondScreenActivity.kt
@@ -0,0 +1,90 @@
+package com.example.mpandroidchart
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.mpandroidchart.Constants.FIXED_INCOME
+import com.example.mpandroidchart.databinding.ActivitySecondScreenBinding
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+
+class SecondScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondScreenBinding
+
+    private val investmentValue by lazy {
+        intent.getFloatExtra(ARG_INVESTMENT_VALUE, 0F)
+    }
+
+    private val otherInvestmentValue by lazy {
+        intent.getFloatExtra(ARG_OTHER_INVESTMENTS, 0F)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupPieChart()
+    }
+
+    private fun setupPieChart() {
+        setupPieChartConfigs()
+        binding.mainPieChart.data = getPieData()
+    }
+
+    private fun setupPieChartConfigs() {
+        binding.mainPieChart.apply {
+            isDrawHoleEnabled = true
+            setHoleColor(Color.TRANSPARENT)
+            setDrawEntryLabels(false)
+            holeRadius = 85f
+            centerText = setUpCenterText()
+            setCenterTextSize(24F)
+            description.isEnabled = false
+            legend.isEnabled = false
+            setTouchEnabled(false)
+        }
+    }
+
+    private fun setUpCenterText(): CharSequence {
+        val percentageValue = (investmentValue / (investmentValue + otherInvestmentValue)) * 100
+        return String.format("%.0f%%", percentageValue)
+    }
+
+    private fun getPieData(): PieData {
+        val pieDataSet = PieDataSet(getPieDataSet(), "").apply {
+            setDrawValues(false)
+            colors = listOf(
+                ContextCompat.getColor(applicationContext, R.color.teal_700),
+                ContextCompat.getColor(applicationContext, R.color.cinza_claro)
+            )
+            sliceSpace = 3f
+        }
+        return PieData(pieDataSet)
+    }
+
+    private fun getPieDataSet(): ArrayList<PieEntry> {
+        return arrayListOf<PieEntry>().apply {
+            add(PieEntry(investmentValue, FIXED_INCOME))
+            add(PieEntry(otherInvestmentValue, ARG_OTHER_INVESTMENTS))
+        }
+    }
+
+    companion object {
+        private const val ARG_OTHER_INVESTMENTS = "outros investimentos"
+        private const val ARG_INVESTMENT_VALUE = "investment value"
+
+        fun newInstance(context: Context, totalValue: Float, investmentValue: Float) {
+            val intent = Intent(context, SecondScreenActivity::class.java).apply {
+                putExtra(ARG_OTHER_INVESTMENTS, totalValue)
+                putExtra(ARG_INVESTMENT_VALUE, investmentValue)
+            }
+            context.startActivity(intent)
+        }
+    }
+}
